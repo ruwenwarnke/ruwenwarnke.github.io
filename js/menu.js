583e177954d668fe3079a1d6f926c64abcba05c8
ruwenwarnke <warnke.ruwen@googlemail.com>
2016-02-18 12:21:40 +0100

    First Commit

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+
+
+
+
+$(function() {
+  $('#menu').hover(function() {
+    $('#myCarousel').css('transition', 'opacity 1s');
+    $('#myCarousel').css('opacity', '0.2');
+    $('#menu').css('opacity','1!important');		
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#myCarousel').css('opacity', '1');
+  });
+});
+
+
+
