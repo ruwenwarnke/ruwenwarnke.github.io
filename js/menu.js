f8c2fa266b332803ed24630bdd3043ceefb88d68
ruwenwarnke <warnke.ruwen@googlemail.com>
2016-02-26 18:03:19 +0100

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -6,12 +6,13 @@ $(function() {
   $('#menu').hover(function() {
     $('#myCarousel').css('transition', 'opacity 1s');
     $('#myCarousel').css('opacity', '0.2');
-    $('#menu').css('opacity','1!important');		
+    $('#menu').css('opacity','1!important');	
   }, function() {
     // on mouseout, reset the background colour
     $('#myCarousel').css('opacity', '1');
   });
 });
+
 
 
 
